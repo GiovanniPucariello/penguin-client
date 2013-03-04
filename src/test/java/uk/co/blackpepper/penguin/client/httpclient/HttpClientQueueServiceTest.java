8334a060862de4234e5b29444a01e0b17a4a1ad2
Mark Hobson <markhobson@gmail.com>
2013-03-04 21:36:00 +0000

    Simplified tests

--- src/test/java/uk/co/blackpepper/penguin/client/httpclient/HttpClientQueueServiceTest.java
+++ src/test/java/uk/co/blackpepper/penguin/client/httpclient/HttpClientQueueServiceTest.java
@@ -3,9 +3,6 @@ package uk.co.blackpepper.penguin.client.httpclient;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,6 +24,10 @@ import org.junit.Test;
 import uk.co.blackpepper.penguin.client.Queue;
 import uk.co.blackpepper.penguin.client.ServiceException;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
@@ -36,55 +37,47 @@ public class HttpClientQueueServiceTest
 {
     private HttpClient client;
     
-    private String apiUrl;
-    
     private HttpClientQueueService service;
     
     @Before
     public void setUp()
     {
         client = mock(HttpClient.class);
-        apiUrl = "http://localhost:8080/api";
-        
-        service = new HttpClientQueueService(client, apiUrl);
+        service = new HttpClientQueueService(client, "http://localhost:8080/api");
     }
     
     @Test
     public void getAllWhenNoQueues() throws ServiceException, IOException, URISyntaxException
     {
         when(client.execute(argThat(matchesRequest("GET", "http://localhost:8080/api/queues"))))
-            .thenReturn(createJsonResponse("[]"));
+            .thenReturn(jsonResponse("[]"));
         
-        assertEquals(Collections.emptyList(), service.getAll());
+        assertEquals(emptyList(), service.getAll());
     }
     
     @Test
     public void getAllWhenQueue() throws ServiceException, IOException, URISyntaxException
     {
         when(client.execute(argThat(matchesRequest("GET", "http://localhost:8080/api/queues"))))
-            .thenReturn(createJsonResponse("[{_id: 1, name: A}]"));
+            .thenReturn(jsonResponse("[{_id: 1, name: A}]"));
         
-        List<Queue> expecteds = Collections.singletonList(new Queue("1", "A"));
-        
-        assertEquals(expecteds, service.getAll());
+        assertEquals(singletonList(new Queue("1", "A")), service.getAll());
     }
     
     @Test
     public void getAllWhenQueues() throws ServiceException, IOException, URISyntaxException
     {
         when(client.execute(argThat(matchesRequest("GET", "http://localhost:8080/api/queues"))))
-            .thenReturn(createJsonResponse("[{_id: 1, name: A}, {_id: 2, name: B}]"));
+            .thenReturn(jsonResponse("[{_id: 1, name: A}, {_id: 2, name: B}]"));
         
-        List<Queue> expecteds = Arrays.asList(new Queue("1", "A"), new Queue("2", "B"));
-        
-        assertEquals(expecteds, service.getAll());
+        assertEquals(asList(new Queue("1", "A"), new Queue("2", "B")), service.getAll());
     }
     
     @Test(expected = ServiceException.class)
     public void getAllWhenNotFound() throws ServiceException, IOException, URISyntaxException
     {
         when(client.execute(argThat(matchesRequest("GET", "http://localhost:8080/api/queues"))))
-            .thenReturn(createNotFound());
+            .thenReturn(notFound());
         
         service.getAll();
     }
@@ -125,17 +118,17 @@ public class HttpClientQueueServiceTest
 		};
     }
     
-    private static HttpResponse createJsonResponse(String json)
+    private static HttpResponse jsonResponse(String json)
     {
-        return createResponse(new StringEntity(json, ContentType.APPLICATION_JSON));
+        return response(new StringEntity(json, ContentType.APPLICATION_JSON));
     }
     
-    private static HttpResponse createResponse(HttpEntity entity)
+    private static HttpResponse response(HttpEntity entity)
     {
-        return createResponse(HttpStatus.SC_OK, entity);
+        return response(HttpStatus.SC_OK, entity);
     }
     
-    private static HttpResponse createResponse(int statusCode, HttpEntity entity)
+    private static HttpResponse response(int statusCode, HttpEntity entity)
     {
         ProtocolVersion version = new ProtocolVersion("HTTP", 1, 0);
 		StatusLine statusLine = new BasicStatusLine(version, statusCode, null);
@@ -145,9 +138,9 @@ public class HttpClientQueueServiceTest
         return response;
     }
     
-    private static HttpResponse createNotFound()
+    private static HttpResponse notFound()
     {
-        return createResponse(HttpStatus.SC_NOT_FOUND, null);
+        return response(HttpStatus.SC_NOT_FOUND, null);
     }
     
 }
