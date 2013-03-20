d0683514588112c504b2a77ab918090f4b7b98fc
Mark Hobson <markhobson@gmail.com>
2013-03-20 21:42:57 +0000

    Added Queue.getStoryCount tests

--- src/test/java/uk/co/blackpepper/penguin/client/QueueTest.java
+++ src/test/java/uk/co/blackpepper/penguin/client/QueueTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
 import static org.junit.Assert.assertEquals;
@@ -63,14 +64,126 @@ public class QueueTest
 	@Test(expected = UnsupportedOperationException.class)
 	public void getStoriesIsImmutable()
 	{
-		createQueue().getStories().clear();
+		createQueueWithStories().getStories().clear();
+	}
+	
+	@Test
+	public void getStoryCountMergedWithNoStories()
+	{
+		Queue queue = createQueue();
+		
+		assertEquals(0, queue.getStoryCount(true));
+	}
+	
+	@Test
+	public void getStoryCountMergedWithMergedStory()
+	{
+		Story story = new Story("1", "P", "Q", "R", true);
+		Queue queue = createQueue(story);
+		
+		assertEquals(1, queue.getStoryCount(true));
+	}
+	
+	@Test
+	public void getStoryCountMergedWithMergedStories()
+	{
+		Story story1 = new Story("1", "P", "Q", "R", true);
+		Story story2 = new Story("2", "S", "T", "U", true);
+		Queue queue = createQueue(story1, story2);
+		
+		assertEquals(2, queue.getStoryCount(true));
+	}
+	
+	@Test
+	public void getStoryCountMergedWithUnmergedStory()
+	{
+		Story story = new Story("1", "P", "Q", "R", false);
+		Queue queue = createQueue(story);
+		
+		assertEquals(0, queue.getStoryCount(true));
+	}
+	
+	@Test
+	public void getStoryCountMergedWithUnmergedStories()
+	{
+		Story story1 = new Story("1", "P", "Q", "R", false);
+		Story story2 = new Story("2", "S", "T", "U", false);
+		Queue queue = createQueue(story1, story2);
+		
+		assertEquals(0, queue.getStoryCount(true));
+	}
+	
+	@Test
+	public void getStoryCountMergedWithMergedAndUnmergedStories()
+	{
+		Story story1 = new Story("1", "P", "Q", "R", true);
+		Story story2 = new Story("2", "S", "T", "U", false);
+		Queue queue = createQueue(story1, story2);
+		
+		assertEquals(1, queue.getStoryCount(true));
+	}
+	
+	@Test
+	public void getStoryCountUnmergedWithNoStories()
+	{
+		Queue queue = createQueue();
+		
+		assertEquals(0, queue.getStoryCount(false));
+	}
+	
+	@Test
+	public void getStoryCountUnmergedWithMergedStory()
+	{
+		Story story = new Story("1", "P", "Q", "R", true);
+		Queue queue = createQueue(story);
+		
+		assertEquals(0, queue.getStoryCount(false));
+	}
+	
+	@Test
+	public void getStoryCountUnmergedWithMergedStories()
+	{
+		Story story1 = new Story("1", "P", "Q", "R", true);
+		Story story2 = new Story("2", "S", "T", "U", true);
+		Queue queue = createQueue(story1, story2);
+		
+		assertEquals(0, queue.getStoryCount(false));
+	}
+	
+	@Test
+	public void getStoryCountUnmergedWithUnmergedStory()
+	{
+		Story story = new Story("1", "P", "Q", "R", false);
+		Queue queue = createQueue(story);
+		
+		assertEquals(1, queue.getStoryCount(false));
+	}
+	
+	@Test
+	public void getStoryCountUnmergedWithUnmergedStories()
+	{
+		Story story1 = new Story("1", "P", "Q", "R", false);
+		Story story2 = new Story("2", "S", "T", "U", false);
+		Queue queue = createQueue(story1, story2);
+		
+		assertEquals(2, queue.getStoryCount(false));
+	}
+	
+	@Test
+	public void getStoryCountUnmergedWithMergedAndUnmergedStories()
+	{
+		Story story1 = new Story("1", "P", "Q", "R", true);
+		Story story2 = new Story("2", "S", "T", "U", false);
+		Queue queue = createQueue(story1, story2);
+		
+		assertEquals(1, queue.getStoryCount(false));
 	}
 	
 	@Test
 	public void hashCodeWhenEqual()
 	{
-		Queue queue1 = createQueue();
-		Queue queue2 = createQueue();
+		Queue queue1 = createQueueWithStories();
+		Queue queue2 = createQueueWithStories();
 		
 		assertEquals("hashCode", queue1.hashCode(), queue2.hashCode());
 	}
@@ -78,8 +191,8 @@ public class QueueTest
 	@Test
 	public void equalsWhenEqual()
 	{
-		Queue queue1 = createQueue();
-		Queue queue2 = createQueue();
+		Queue queue1 = createQueueWithStories();
+		Queue queue2 = createQueueWithStories();
 		
 		assertTrue("equals", queue1.equals(queue2));
 	}
@@ -129,9 +242,14 @@ public class QueueTest
 		assertEquals("stories", expectedStories, actual.getStories());
 	}
 	
-	private static Queue createQueue()
+	private static Queue createQueueWithStories()
 	{
-		return new Queue("1", "A", createStories());
+		return createQueue(createStory());
+	}
+	
+	private static Queue createQueue(Story... stories)
+	{
+		return new Queue("1", "A", asList(stories));
 	}
 	
 	private static List<Story> createStories()
