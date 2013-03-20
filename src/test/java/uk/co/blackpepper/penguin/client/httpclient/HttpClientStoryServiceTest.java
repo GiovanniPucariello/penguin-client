e0733fede89662e47656ece4a1d5feb45600fbb9
Mark Hobson <markhobson@gmail.com>
2013-03-20 20:42:03 +0000

    Made HttpClientStoryService tests more explicit by inlining inputs to visually correlate with expectations

--- src/test/java/uk/co/blackpepper/penguin/client/httpclient/HttpClientStoryServiceTest.java
+++ src/test/java/uk/co/blackpepper/penguin/client/httpclient/HttpClientStoryServiceTest.java
@@ -1,14 +1,5 @@
 package uk.co.blackpepper.penguin.client.httpclient;
 
-import static java.util.Collections.emptyList;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.argThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static uk.co.blackpepper.penguin.client.httpclient.HttpRequests.matchesGet;
-import static uk.co.blackpepper.penguin.client.httpclient.HttpResponses.json;
-import static uk.co.blackpepper.penguin.client.httpclient.HttpResponses.notFound;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -20,58 +11,21 @@ import org.junit.Test;
 import uk.co.blackpepper.penguin.client.ServiceException;
 import uk.co.blackpepper.penguin.client.Story;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static uk.co.blackpepper.penguin.client.httpclient.HttpRequests.matchesGet;
+import static uk.co.blackpepper.penguin.client.httpclient.HttpResponses.json;
+import static uk.co.blackpepper.penguin.client.httpclient.HttpResponses.notFound;
+
 public class HttpClientStoryServiceTest
 {
-	private static final String BASE_URL = "http://localhost/api";
-	private static final String QUEUE_ID = "513c58da4df5b49e3d000001";
-	private static final String GET_ALL_REQUEST_URL = BASE_URL + "/queue/" + QUEUE_ID;
-	private static final String STORY_ID_1 = "513c5eef4df5b49e3d000002";
-	private static final String STORY_ID_2 = "513c5eef4df5b49e3d000003";
-	
-	private static final String EMPTY_QUEUE = 
-		"{ " +
-		" \"name\": \"Queue\", " +
-		"   \"stories\": [], " +
-		"   \"_id\": \"513c58da4df5b49e3d000001\" " +
-		" }";
-	
-	private static final String QUEUE_WITH_ONE_STORY =
-		"{ " +
-		" \"_id\": \"513c58da4df5b49e3d000001\", " +
-		"  \"name\": \"Queue\", " +
-		"  \"stories\": [ " +
-		"    { " +
-		"      \"reference\": \"S-1\", " +
-		"      \"title\": \"T1\", " +
-		"      \"author\": \"A1\", " +
-		"      \"merged\": false, " +
-		"      \"_id\": \"513c5eef4df5b49e3d000002\" " +
-		"    } " +
-		"  ] " +
-		"}";
-
-	private static final String QUEUE_WITH_MULTIPLE_STORIES =
-		"{ " +
-		" \"_id\": \"513c58da4df5b49e3d000001\", " +
-		"  \"name\": \"Queue\", " +
-		"  \"stories\": [ " +
-		"    { " +
-		"      \"reference\": \"S-1\", " +
-		"      \"title\": \"T1\", " +
-		"      \"author\": \"A1\", " +
-		"      \"merged\": false, " +
-		"      \"_id\": \"513c5eef4df5b49e3d000002\" " +
-		"    }, " +
-		"    { " +
-		"      \"reference\": \"S-2\", " +
-		"      \"title\": \"T2\", " +
-		"      \"author\": \"A2\", " +
-		"      \"merged\": true, " +
-		"      \"_id\": \"513c5eef4df5b49e3d000003\" " +
-		"    } " +
-		"  ] " +
-		"}";
-
 	private HttpClient client;
 
 	private HttpClientStoryService service;
@@ -80,35 +34,43 @@ public class HttpClientStoryServiceTest
 	public void setUp()
 	{
 		client = mock(HttpClient.class);
-		service = new HttpClientStoryService(client, BASE_URL);
+		service = new HttpClientStoryService(client, "http://localhost/api");
 	}
 
 	@Test
 	public void getAllWhenNoStories() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
-			.thenReturn(json(EMPTY_QUEUE));
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
+			.thenReturn(json("{_id: 1, name: A, stories: []}"));
 
-		assertEquals(emptyList(), service.getAll(QUEUE_ID));
+		assertEquals(emptyList(), service.getAll("1"));
 	}
 
 	@Test
 	public void getAllWhenStory() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
-			.thenReturn(json(QUEUE_WITH_ONE_STORY));
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
+			.thenReturn(json("{_id: 1, name: A, stories: ["
+				+ "{_id: 2, reference: P, title: Q, author: R, merged: false}"
+				+ "]}"));
 
-		assertEquals(new Story(STORY_ID_1, "S-1", "T1", "A1", false), service.getAll(QUEUE_ID).get(0));
+		assertEquals(new Story("2", "P", "Q", "R", false), service.getAll("1").get(0));
 	}
 
 	@Test
 	public void getAllWhenStories() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
-			.thenReturn(json(QUEUE_WITH_MULTIPLE_STORIES));
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
+			.thenReturn(json("{_id: 1, name: A, stories: ["
+				+ "{_id: 2, reference: P, title: Q, author: R, merged: false},"
+				+ "{_id: 3, reference: S, title: T, author: U, merged: false}"
+				+ "]}"));
 
-		assertEquals(new Story(STORY_ID_1, "S-1", "T1", "A1", false), service.getAll(QUEUE_ID).get(0));
-		assertEquals(new Story(STORY_ID_2, "S-2", "T2", "A2", true), service.getAll(QUEUE_ID).get(1));
+		List<Story> expecteds = asList(
+			new Story("2", "P", "Q", "R", false),
+			new Story("3", "S", "T", "U", false)
+		);
+		assertEquals(expecteds, service.getAll("1"));
 	}
 
 	@Test(expected = ServiceException.class)
@@ -123,30 +85,30 @@ public class HttpClientStoryServiceTest
 	@Test(expected = ServiceException.class)
 	public void getAllWhenNotFound() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
 			.thenReturn(notFound());
 
-		service.getAll(QUEUE_ID);
+		service.getAll("1");
 	}
 
 	@Test(expected = ServiceException.class)
 	public void getAllWhenIOException() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
 			.thenThrow(new IOException());
 
-		service.getAll(QUEUE_ID);
+		service.getAll("1");
 	}
 
 	@Test
-	public void getMergedStories() throws ServiceException, IOException, URISyntaxException
+	public void getMergedWhenMergedStory() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
-			.thenReturn(json(QUEUE_WITH_MULTIPLE_STORIES));
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
+			.thenReturn(json("{_id: 1, name: A, stories: ["
+				+ "{_id: 2, reference: P, title: Q, author: R, merged: true}"
+				+ "]}"));
 
-		List<Story> results = service.getMerged(QUEUE_ID);
-		assertEquals(1, results.size());
-		assertEquals(new Story(STORY_ID_2, "S-2", "T2", "A2", true), results.get(0));
+		assertEquals(singletonList(new Story("2", "P", "Q", "R", true)), service.getMerged("1"));
 	}
 
 	@Test(expected = ServiceException.class)
@@ -159,14 +121,14 @@ public class HttpClientStoryServiceTest
 	}
 
 	@Test
-	public void getUnmergedStories() throws ServiceException, IOException, URISyntaxException
+	public void getUnmergedWhenUnmergedStory() throws ServiceException, IOException, URISyntaxException
 	{
-		when(client.execute(argThat(matchesGet(GET_ALL_REQUEST_URL))))
-			.thenReturn(json(QUEUE_WITH_MULTIPLE_STORIES));
+		when(client.execute(argThat(matchesGet("http://localhost/api/queue/1"))))
+			.thenReturn(json("{_id: 1, name: A, stories: ["
+				+ "{_id: 2, reference: P, title: Q, author: R, merged: false}"
+				+ "]}"));
 
-		List<Story> results = service.getUnmerged(QUEUE_ID);
-		assertEquals(1, results.size());
-		assertEquals(new Story(STORY_ID_1, "S-1", "T1", "A1", false), results.get(0));
+		assertEquals(singletonList(new Story("2", "P", "Q", "R", false)), service.getUnmerged("1"));
 	}
 
 	@Test(expected = ServiceException.class)
