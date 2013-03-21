3bd65654f4a2751ce518e910bbf0e7b24d4aa674
Mark Hobson <markhobson@gmail.com>
2013-03-21 20:57:42 +0000

    Made constants class non-instantiateable

--- src/main/java/uk/co/blackpepper/penguin/client/httpclient/MediaTypes.java
+++ src/main/java/uk/co/blackpepper/penguin/client/httpclient/MediaTypes.java
@@ -3,4 +3,9 @@ package uk.co.blackpepper.penguin.client.httpclient;
 final class MediaTypes
 {
 	public static final String APPLICATION_JSON = "application/json";
+	
+	private MediaTypes()
+	{
+		throw new AssertionError();
+	}
 }
